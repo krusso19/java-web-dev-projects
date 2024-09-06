b55806766a97f90c98307f6217c6f3d44b608b4d
Katie Russo <krusso196@gmail.com>
2024-09-05 21:09:36 -0400

    Studio Unit 2 Chapter 1 w/ error

---
+++ datatypes/datatypes-studio/src/main/Java/org/launchcode/Area.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a radius");
+        Double radius = input.nextDouble();
+        while (radius <= 0){
+            System.out.println("Invalid radius " + radius + ". Value of radius must be greater than or equal to 0. Enter another number: ");
+            radius = input.nextDouble();
+        }
+
+        Double area = Circle.getArea(radius);
+        System.out.print("The area of the circle with a radius of "+ radius +" is: "+ area);
+    }
+}
