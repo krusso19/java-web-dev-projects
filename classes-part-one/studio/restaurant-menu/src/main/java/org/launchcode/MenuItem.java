8dbde364c505ae75b3c4c8030d58d4dc8377972b
Katie Russo <krusso196@gmail.com>
2024-09-12 21:32:41 -0400

    Class 3 studio MenuItems

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -0,0 +1,53 @@
+package org.launchcode;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private Boolean isNew;
+//want to use primitive
+    private MenuItem(String name, double price, String description, String category, Boolean isNew) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setName(double aPrice) {
+        price = aPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String aDescription) {
+        description = aDescription;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String aCategory) {
+        category = aCategory;
+    }
+
+    public Boolean getIsNew(){
+        return isNew;
+    }
+    public void setIsNew(Boolean aIsNew){
+        isNew = aIsNew;
+    }
+
+}/*can be copied for all of the key/value pairs*/
