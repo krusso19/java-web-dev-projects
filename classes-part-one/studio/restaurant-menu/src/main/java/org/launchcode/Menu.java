c2a0c5a85c782568f892bd0573c57008eb565b4a
Katie Russo <krusso196@gmail.com>
2024-09-16 21:33:52 -0400

    Class 4 Studio ... maybe about 50% completed?

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,4 +1,32 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+
 public class Menu {
+    ArrayList<MenuItem> menuList = new ArrayList<>();
+    String updated;
+
+    public Menu(ArrayList<MenuItem> menuList, String updated) {
+        this.menuList = menuList;
+        this.updated = updated;
+    }
+
+    public void setItem(MenuItem aMenuItem, String aUpdate) {
+        menuList.add(aMenuItem);
+        setUpdated(aUpdate);
+        aMenuItem.setIsNew(true);
+    }
+
+    public void setUpdated(String anUpdate) {
+        for (MenuItem list : menuList) {
+            if (list.getIsNew()) {
+                list.setIsNew(false);
+            }
+        }
+    }
+
+//    public ArrayList getMenuList() {
+//        System.out.println(menuList);
+//    }
+
 }
