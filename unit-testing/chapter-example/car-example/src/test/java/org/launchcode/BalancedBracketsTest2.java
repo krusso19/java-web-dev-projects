a6db74eaa3446ad31860d8c73708a59f83a680b6
Katie Russo <krusso196@gmail.com>
2024-09-19 21:31:01 -0400

    Chp 6 Studio Room 8

---
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/BalancedBracketsTest2.java
@@ -0,0 +1,40 @@
+package org.launchcode;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BalancedBracketsTest {
+    //TODO: add tests here
+
+//BalancedBracket test_balancedBracket = new BalancedBrackets("Launch[code]")   ;
+
+    @Test
+    public void emptyTest() {
+        String message = "balanced brackets around characters returned true";
+        String testData = "[LaunchCode]";
+        boolean result = BalancedBracketsTwo.hasBalancedBrackets(testData);
+        assertTrue(result, message);
+    }
+    @Test
+    public void noBrackets(){
+        String testData = "sup";
+        boolean result = BalancedBracketsTwo.hasBalancedBrackets(testData);
+        assertTrue(result);
+    }
+    @Test
+    public void oneBracket(){
+        String testData = "[hi";
+       // boolean result = BalancedBracketsTwo.hasBalancedBrackets(testData);
+        assertFalse(BalancedBracketsTwo.hasBalancedBrackets(testData));
+    }
+    @Test
+    public void
+
+}
+
+
+//     * These strings have balanced brackets:
+//        *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+//        *
+//        * While these do not:
+//        *   "[LaunchCode", "Launch]Code[", "[", "]["
