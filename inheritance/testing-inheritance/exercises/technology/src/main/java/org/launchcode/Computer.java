bf8bdf9094a4dc95f01acb26af460600b1a563c7
Katie Russo <krusso196@gmail.com>
2024-09-26 15:53:37 -0400

    Class 6 Chapter 7 Exercises Inheritance (incomplete)

---
+++ inheritance/testing-inheritance/exercises/technology/src/main/java/org/launchcode/Computer.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+
+public class Computer {
+    String owner;
+    String brand;
+    int year;
+    Integer currentDate = 2024;
+
+    public Computer (String owner, String brand, int year){
+        this.owner = owner;
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public Integer age(){
+    }
+}
